2ea1b0a080d1e9c973c517b1b48bbf2e735cc5e2
Maxime Buffa <mbuffa@gmail.com>
2020-12-10 13:10:57 +0100

    F it.

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, fs};
 use std::iter::FromIterator;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Instruction {
   operator: String,
   args: Vec<char>
@@ -13,8 +13,7 @@ fn main() {
 
   let instructions = compute_instructions(contents);
 
-  let mut trace: HashMap<usize, u8> = HashMap::new();
-  let acc: i64 = evaluate(&instructions, 0, &mut trace, 0);
+  let acc = debug(instructions);
 
   // println!("{:?}", instructions);
   println!("{}", acc);
@@ -37,64 +36,115 @@ fn compute_instructions(buffer: String) -> Vec<Instruction> {
   instructions
 }
 
-fn evaluate(instructions: &Vec<Instruction>, idx: usize, trace: &mut HashMap<usize, u8>, acc: i64) -> i64 {
+fn swap_instructions(
+  instructions: &Vec<Instruction>,
+  left: &(usize, String),
+  right: &(usize, String)
+) -> Vec<Instruction> {
+    let mut copy = instructions.clone();
+
+    let left_instruction = instructions.get(left.0).unwrap();
+    let right_instruction = instructions.get(right.0).unwrap();
+
+    std::mem::replace(
+      &mut copy[left.0],
+      Instruction {
+        operator: right_instruction.operator.clone(),
+        args: left_instruction.args.clone()
+      }
+    );
+
+    std::mem::replace(
+      &mut copy[right.0],
+      Instruction {
+        operator: left_instruction.operator.clone(),
+        args: right_instruction.args.clone()
+      }
+    );
+
+    copy
+  }
+
+fn debug(
+  instructions: Vec<Instruction>,
+) -> i64 {
+  let mut trace: HashMap<usize, u8> = HashMap::new();
+  let mut to_swap_ids: Vec<(usize, String)> = Vec::new();
+  let mut dummy: Vec<(usize, String)> = Vec::new();
+  let acc = 0;
+
+  let (result, mut to_swap_ids, success) =
+    evaluate(&instructions, 0, &mut trace, acc, &mut to_swap_ids);
+
+  println!("0: {} {:?} {}", result, to_swap_ids, success);
+
+  if success || to_swap_ids.len() == 0 {
+    return result;
+  }
+
+  let mut l: usize = 0;
+  let mut r = to_swap_ids.len() - 1;
+
+  while l != r {
+    let left = to_swap_ids[l].clone();
+    let right = to_swap_ids[r].clone();
+
+    if left != right {
+      to_swap_ids[l] = right.clone();
+      to_swap_ids[r] = left.clone();
+
+      let instructions_copy = swap_instructions(
+        &instructions,
+        &left,
+        &right
+      );
+      trace.clear();
+
+      let (result, _, success) =
+        evaluate(&instructions_copy, 0, &mut trace, acc, &mut dummy);
+
+      if success {
+        return result;
+      }
+    } else {
+      l += 1;
+      r -= 1;
+    }
+  }
+
+  result
+}
+
+fn evaluate(
+  instructions: &Vec<Instruction>,
+  idx: usize,
+  trace: &mut HashMap<usize, u8>,
+  acc: i64,
+  to_swap_ids: &mut Vec<(usize, String)>
+) -> (i64, Vec<(usize, String)>, bool) {
   let current = &instructions[idx];
 
   println!("{} {:?}", current.operator, current.args);
 
   if already_evaluated(idx, trace) {
     println!("Terminating.");
-    return acc;
+    return (acc, to_swap_ids.clone(), false);
   }
 
   trace.insert(idx, 1);
 
   if current.operator == "nop" {
-    // println!("nop");
-    if already_evaluated(idx + 1, trace) {
-      println!("Already evaluated nop next");
-      let (sign, offset) = extract(&current.args);
-
-      if sign == '+' {
-        return evaluate(instructions, idx + (offset as usize), trace, acc);
-      } else if sign == '-' {
-        return evaluate(instructions, idx - (offset as usize), trace, acc);
-      }
-    } else {
-      return evaluate(instructions, idx + 1, trace, acc);
-    }
+    to_swap_ids.push((idx, current.operator.clone()));
+    return evaluate(instructions, idx + 1, trace, acc, to_swap_ids);
   } else if current.operator == "jmp" {
+    to_swap_ids.push((idx, current.operator.clone()));
     // println!("jmp {:?}", current.args);
     let (sign, offset) = extract(&current.args);
 
     if sign == '+' {
-      if already_evaluated(idx + (offset as usize), trace) {
-        println!("Already evaluated jmp next +");
-        return evaluate(instructions, idx + 1, trace, acc);
-      } else {
-        let range: Vec<usize> = (idx..(offset as usize)).collect();
-
-        // Blacklisting range to replace instruction later.
-        for &step in range.iter() {
-          trace.insert(step, 1);
-        }
-
-        return evaluate(instructions, idx + (offset as usize), trace, acc);
-      }
+        return evaluate(instructions, idx + (offset as usize), trace, acc, to_swap_ids);
     } else if sign == '-' {
-      if already_evaluated(idx - (offset as usize), trace) {
-        println!("Already evaluated jmp next -");
-        return evaluate(instructions, idx + 1, trace, acc);
-      } else {
-        let range: Vec<usize> = ((idx - offset as usize)..(idx)).collect();
-
-        // Blacklisting range to replace instruction later.
-        for &step in range.iter() {
-          trace.insert(step, 1);
-        }
-
-        return evaluate(instructions, idx - (offset as usize), trace, acc);
-      }
+      return evaluate(instructions, idx - (offset as usize), trace, acc, to_swap_ids);
     }
   } else if current.operator == "acc" {
     // println!("acc {}", acc);
@@ -109,18 +159,19 @@ fn evaluate(instructions: &Vec<Instruction>, idx: usize, trace: &mut HashMap<usi
     }
 
     if idx == instructions.len() - 1 {
-      return new_acc;
+      println!("Returning {} {:?} {}", new_acc, to_swap_ids, true);
+      return (new_acc, to_swap_ids.clone(), true);
     }
 
     if sign == '+' {
-      return evaluate(instructions, idx + 1, trace, new_acc);
+      return evaluate(instructions, idx + 1, trace, new_acc, to_swap_ids);
     } else if sign == '-' {
-      println!("{} {}", acc, offset);
-      return evaluate(instructions, idx + 1, trace, new_acc);
+      // println!("{} {}", acc, offset);
+      return evaluate(instructions, idx + 1, trace, new_acc, to_swap_ids);
     }
   }
 
-  acc
+  (acc, to_swap_ids.clone(), false)
 }
 
 fn extract(args: &Vec<char>) -> (char, i64) {
